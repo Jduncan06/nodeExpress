05d887a875f4b85a950f54ab9308ebfcf73ba804
Justin Duncan <floydfan_06@yahoo.com>
2022-11-05 11:22:05 -0700

    workshop1

--- 5-NodeJS-Express-MongoDB/node-express/routes/partnerRouter.js
+++ 5-NodeJS-Express-MongoDB/node-express/routes/partnerRouter.js
@@ -1,49 +1,54 @@
-const express = require('express');
+const express = require("express");
 const partnerRouter = express.Router();
 
-partnerRouter.route('/partners')
-.all((req, res, next) => {
+partnerRouter
+  .route("/")
+  .all((req, res, next) => {
     res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
+    res.setHeader("Content-Type", "text/plain");
     next();
-})
-.get((req, res) => {
-    res.end('Will send all the partners to you');
-})
-.post((req, res) => {
-    res.end(`Will add the partner: ${req.body.name} with description: ${req.body.description}`);
-})
-.put((req, res) => {
+  })
+  .get((req, res) => {
+    res.end("Will send all the partners to you");
+  })
+  .post((req, res) => {
+    res.end(
+      `Will add the partner: ${req.body.name} with description: ${req.body.description}`
+    );
+  })
+  .put((req, res) => {
     res.statusCode = 403;
-    res.end('PUT operation not supported on /partner');
-})
-.delete((req, res) => {
-    res.end('Deleting all partners');
-});
+    res.end("PUT operation not supported on /partner");
+  })
+  .delete((req, res) => {
+    res.end("Deleting all partners");
+  });
 
-partnerRouter.route('/partners/:partnerId')
+partnerRouter
+  .route("/:partnerId")
 
-.all((req, res, next) => {
+  .all((req, res, next) => {
     res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
+    res.setHeader("Content-Type", "text/plain");
     next();
-})
+  })
 
-.get((req, res) => {
+  .get((req, res) => {
     res.end(`Will send details of the partner: ${req.params.partnerId} to you`);
-})
-.post((req, res) => {
+  })
+  .post((req, res) => {
     res.statusCode = 403;
-    res.end(`POST operation not supported on /partners/${req.params.partnerId}`);
-})
-.put((req, res) => {
+    res.end(
+      `POST operation not supported on /partners/${req.params.partnerId}`
+    );
+  })
+  .put((req, res) => {
     res.write(`Updating the partner: ${req.params.partnerId}\n`);
     res.end(`Will update the partner: ${req.body.name}
         with description: ${req.body.description}`);
-})
-.delete((req, res) => {
+  })
+  .delete((req, res) => {
     res.end(`Deleting partner: ${req.params.partnerId}`);
-});
-
+  });
 
 module.exports = partnerRouter;
