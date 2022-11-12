14a7bfe9a50983a851bd284a97f18d14e78d557d
Justin Duncan <floydfan_06@yahoo.com>
2022-11-12 09:54:46 -0800

    workshop 2

---
+++ 5-NodeJS-Express-MongoDB/nucampsiteServer/routes/promotionRouter.js
@@ -0,0 +1,70 @@
+const express = require("express");
+const Promotion = require("../models/promotion");
+
+const promotionRouter = express.Router();
+
+promotionRouter
+  .route("/")
+  .get((req, res, next) => {
+    Promotion.find()
+      .then((promotions) => {
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "application/json");
+        res.json(promotions);
+      })
+      .catch((err) => next(err));
+  })
+  .post((req, res, next) => {
+    Promotion.create(req.body)
+      .then((promotion) => {
+        console.log("Promotion Created ", promotion);
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "application/json");
+        res.json(promotion);
+      })
+      .catch((err) => next(err));
+  })
+  .put((req, res) => {
+    res.statusCode = 403;
+    res.end("PUT operation not supported on /promotions");
+  })
+  .delete((req, res, next) => {
+    Promotion.deleteMany()
+      .then((response) => {
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "application/json");
+        res.json(response);
+      })
+      .catch((err) => next(err));
+  });
+
+promotionRouter
+  .route("/:promotionId")
+
+  .all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/plain");
+    next();
+  })
+
+  .get((req, res) => {
+    res.end(
+      `Will send details of the promotion: ${req.params.promotionId} to you`
+    );
+  })
+  .post((req, res) => {
+    res.statusCode = 403;
+    res.end(
+      `POST operation not supported on /promotion/${req.params.promotionId}`
+    );
+  })
+  .put((req, res) => {
+    res.write(`Updating the promotion: ${req.params.promotionId}\n`);
+    res.end(`Will update the promotion: ${req.body.name}
+        with description: ${req.body.description}`);
+  })
+  .delete((req, res) => {
+    res.end(`Deleting promotion: ${req.params.promotionId}`);
+  });
+
+module.exports = promotionRouter;
